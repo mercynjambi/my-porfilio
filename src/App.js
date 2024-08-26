3572b1bcb7911a417095345e697990a86a9d4f68
mercynjambi <njambim695@gmail.com>
2024-08-26 22:35:32 +0300

    created the navbar component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "./components/Navbar";
+
+import './styles.css';
+
+
+function App() {
+  return (
+    <div className="app">
+        
+        <Navbar/>
+        
+    </div>
+  )
+}
+
+export default App
